0f9f2b1c3d78519bd31117d4ef844383aaca1687
Sam Tran <samtran0331@gmail.com>
2022-01-10 14:55:37 -0500

    734-1-4792 - Restrict ability to delete an asset

--- Advantage Database/Programmability/Stored Procedures/Procs1/advsp_proofing_can_delete_document.proc.sql
+++ Advantage Database/Programmability/Stored Procedures/Procs1/advsp_proofing_can_delete_document.proc.sql
@@ -1,7 +1,10 @@
-IF EXISTS ( SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[advsp_proofing_can_delete_document]') AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
-BEGIN
-    DROP PROCEDURE [dbo].[advsp_proofing_can_delete_document];
-END
+IF EXISTS (
+		SELECT *
+		FROM dbo.sysobjects
+		WHERE id = object_id(N'[dbo].[advsp_proofing_can_delete_document]')
+			AND OBJECTPROPERTY(id, N'IsProcedure') = 1
+		)
+	DROP PROCEDURE [dbo].[advsp_proofing_can_delete_document]
 GO
 CREATE PROCEDURE [dbo].[advsp_proofing_can_delete_document] 
 @ALERT_ID INT,
@@ -9,97 +12,104 @@ CREATE PROCEDURE [dbo].[advsp_proofing_can_delete_document]
 AS
 /*=========== QUERY ===========*/
 BEGIN
+
 	--	VARS
 	BEGIN
 		DECLARE
 			@IS_PROOF BIT = 0,
 			@CAN_DELETE BIT = 1,
-			@MESSAGE VARCHAR(MAX) = '',
-			@MARKUP_COUNT INT = 0,
-			@COMMENT_COUNT INT = 0
+			@CAN_DELETE_MESSAGE VARCHAR(MAX),
+			@TOTAL_MARKUP_COUNT INT = 0,
+			@TOTAL_COMMENT_COUNT INT = 0,
+			@APPROVAL_COUNT INT
 		;
 	END
-	--	INIT
+
+	--  INIT
 	BEGIN
 		SELECT
-			@IS_PROOF = CASE
+			@IS_PROOF =	CASE
 							WHEN A.ALERT_CAT_ID = 79 THEN 1
 							ELSE 0
 						END
-		FROM	
+		FROM
 			ALERT A WITH(NOLOCK)
 		WHERE
 			A.ALERT_ID = @ALERT_ID
 		;
-		IF @IS_PROOF = 1
-		BEGIN
-			SELECT 
-				@MARKUP_COUNT = COUNT(1) 
-			FROM 
-				PROOFING_MARKUP PM WITH(NOLOCK) 
-			WHERE 
-				PM.DOCUMENT_ID = @DOCUMENT_ID
-			;
-			SELECT 
-				@COMMENT_COUNT = COUNT(1) 
-			FROM 
-				ALERT_COMMENT AC WITH(NOLOCK) 
-			WHERE 
-				AC.DOCUMENT_ID = @DOCUMENT_ID
-			;
-		END
 	END
-	--	MESSAGE
+	--  WORK
 	BEGIN
 		IF @IS_PROOF = 1
 		BEGIN
-			IF @MARKUP_COUNT > 0 --OR @COMMENT_COUNT > 0
+		SELECT 
+			@TOTAL_MARKUP_COUNT = X.TotalMarkups,
+			@TOTAL_COMMENT_COUNT = X.TotalComments,
+			@APPROVAL_COUNT = X.TotalApproves + X.TotalRejects + X.TotalDefers
+		FROM
+			[dbo].[advtf_proofing_get_documents_details] (@ALERT_ID) X
+		WHERE
+			X.DocumentID = @DOCUMENT_ID
+		;
+		END
+	END
+
+	--  MESSAGE
+	BEGIN
+		IF @CAN_DELETE = 1
+		BEGIN
+			IF @APPROVAL_COUNT > 0 
 			BEGIN
 				SELECT @CAN_DELETE = 0;
-				IF @MARKUP_COUNT > 0
+				IF @APPROVAL_COUNT = 1
 				BEGIN
-					IF @MARKUP_COUNT = 1
-					BEGIN
-						SELECT @MESSAGE = 'This asset has one markup and cannot be deleted.'
-					END
-					IF @MARKUP_COUNT > 1 
-					BEGIN
-						SELECT @MESSAGE = 'This asset has ' + CAST(@MARKUP_COUNT AS VARCHAR) + ' markups and cannot be deleted';
-					END
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has one approval and cannot be deleted.'
 				END
-				--IF @COMMENT_COUNT > 0 
-				--BEGIN
-				--	IF @MARKUP_COUNT > 0
-				--	BEGIN
-				--		SELECT @MESSAGE = @MESSAGE + '  ';
-				--	END
-				--	IF @COMMENT_COUNT = 1
-				--	BEGIN
-				--		SELECT @MESSAGE = 'This asset is associated with one comment and cannot be deleted.'
-				--	END
-				--	IF @COMMENT_COUNT > 1 
-				--	BEGIN
-				--		SELECT @MESSAGE = 'This asset is associated with ' + CAST(@COMMENT_COUNT AS VARCHAR) + ' comments and cannot be deleted';
-				--	END
-				--END
+				ELSE
+				BEGIN
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has ' + CAST(@APPROVAL_COUNT AS VARCHAR)  + ' approvals and cannot be deleted.'
+				END
 			END
 		END
-		ELSE
-		BEGIN	
-			SELECT 
-				@CAN_DELETE = 1, 
-				@MESSAGE = NULL
-			;
+		IF @CAN_DELETE = 1
+		BEGIN
+			IF @TOTAL_MARKUP_COUNT > 0 
+			BEGIN
+				SELECT @CAN_DELETE = 0;
+				IF @TOTAL_MARKUP_COUNT = 1
+				BEGIN
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has one markup and cannot be deleted.'
+				END
+				ELSE
+				BEGIN
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has ' + CAST(@TOTAL_MARKUP_COUNT AS VARCHAR)  + ' markups and cannot be deleted.'
+				END
+			END
+		END
+		IF @CAN_DELETE = 1
+		BEGIN
+			IF @TOTAL_COMMENT_COUNT > 0 
+			BEGIN
+				SELECT @CAN_DELETE = 0;
+				IF @TOTAL_COMMENT_COUNT = 1
+				BEGIN
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has one comment and cannot be deleted.'
+				END
+				ELSE
+				BEGIN
+					SELECT @CAN_DELETE_MESSAGE = 'Asset has ' + CAST(@TOTAL_COMMENT_COUNT AS VARCHAR)  + ' comments and cannot be deleted.'
+				END
+			END
 		END
 	END
 	--	RETURN
 	BEGIN
 		SELECT
-			[CanDelete] = ISNULL(@CAN_DELETE, 1),
-			[Message] =	CASE
-							WHEN ISNULL(@CAN_DELETE, 1) = 1 THEN NULL
-							ELSE @MESSAGE
-						END
+			[CanDelete] = CAST(ISNULL(@CAN_DELETE, 1) AS BIT),
+			[CanDeleteMessage] = @CAN_DELETE_MESSAGE,
+			[TotalMarkupCount] = ISNULL(@TOTAL_MARKUP_COUNT, 0),
+			[TotalCommentCount] = ISNULL(@TOTAL_COMMENT_COUNT, 0),
+			[ApprovalCount] = ISNULL(@APPROVAL_COUNT, 0)
 		;
 	END
 END
